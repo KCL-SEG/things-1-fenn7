57f102edf65b4be592d85a48732b09edc83724fd
fenn7 <afenn666@gmail.com>
2022-10-13 16:23:44 +0100

    validation1

--- things/models.py
+++ things/models.py
@@ -1,8 +1,34 @@
+from enum import unique
 import imp
+from wsgiref.validate import validator
+from django.core.validators import RegexValidator
+from django.core.validators import MinLengthValidator
+from django.core.validators import MaxLengthValidator
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class User(AbstractUser):
-    description = models.TextField
-    quantity = models.BigIntegerField
+class Thing(AbstractUser):
+    username=models.CharField(
+        max_length=30,
+        unique=True,
+        blank=False,
+        validators=[RegexValidator(
+            message="Usernames must be unqiue, "
+        )]
+    )
+    
+    description = models.TextField(
+        max_length=120,
+        unique=False,
+        blank=False,
+    )
+    
+    quantity = models.IntegerField(
+        max_length=100,
+        blank=False,
+        validators=[
+            MinLengthValidator(0, message = "Value must be bigger than 0!"),
+            MaxLengthValidator(100, message = "Value must be less than 100!")
+                    ]
+    )
--- things/tests.py
+++ things/tests.py
@@ -1,3 +1,5 @@
 from django.test import TestCase
 
+from things.models import Thing
+
 # Create your tests here.
