616fc40df69585a5fd72394b30463a71c161b45f
fenn7 <afenn666@gmail.com>
2022-10-13 15:48:03 +0100

    model

--- things/models.py
+++ things/models.py
@@ -1,3 +1,8 @@
+import imp
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    description = models.TextField
+    quantity = models.BigIntegerField
