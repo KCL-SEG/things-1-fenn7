96a7c5ebd80c948631041c6426d5ff5723a28361
fenn7 <afenn666@gmail.com>
2022-10-13 14:50:18 +0100

    html

--- things/views.py
+++ things/views.py
@@ -1,3 +1,9 @@
+import re
+from cgitb import html
+from urllib import response
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return render(request, 'index.html')
