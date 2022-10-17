19d8dd8fd7da701f9ae946ebd41f7dac50dcf87d
fenn7 <afenn666@gmail.com>
2022-10-17 10:32:18 +0100

    remade migrations

--- thingproject/settings.py
+++ thingproject/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import django
+import django
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -36,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles', 'things',
+    'django.contrib.staticfiles', 
+    'things',
 ]
 
 MIDDLEWARE = [
@@ -124,4 +126,4 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = "things.models.Thing"
+AUTH_USER_MODEL = "things.Thing"
