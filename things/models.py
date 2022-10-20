9473fbcc7a750e8f52313fcbeaf425d1353a72b2
fenn7 <k21010668@kcl.ac.uk>
2022-10-20 13:58:49 +0100

    Update models.py
    nouser

--- things/models.py
+++ things/models.py
@@ -8,7 +8,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class Thing(AbstractUser):
+class Thing(models.Model):
     username=models.CharField(
         max_length=30,
         unique=True,
